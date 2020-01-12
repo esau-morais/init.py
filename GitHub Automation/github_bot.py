a95cdc6a1f39d3069ef1bdec7e03f548cafc9cec
esau-morais <esaumorais7@gmail.com>
2020-01-12 15:22:19 -0300

    GitHub Automation Folder added

---
+++ GitHub Automation/github_bot.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+class GitHubBot:
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+    self.driver = webdriver.Chrome()
+
+  
+  def closeBrowser(self):
+    self.driver.close()
+  
+
+  def login(self):
+    # GitHub direct URL
+    driver = self.driver
+    driver.get('https://github.com/')
+    time.sleep(2)
+    # "//a[href='https://github.com/login']"
+    login_btn = driver.find_element_by_xpath("//a[href='https://github.com/login']")
+    login_btn.click()
+    time.sleep(2)
+    # "//input[@name='login']"
+    user = driver.find_element_by_xpath("//input[@name='login']")
+    user.clear()
+    user.send_keys(self.username)
+
+    # "//input[@name='login']"
+    passw = driver.find_element_by_xpath("//input[@name='login']")
+    passw.clear()
+    passw.send_keys(self.password)
+    passw.send_keys(Keys.RETURN)
+    time.sleep(2)
+
+
+esau_ig = GitHubBot('esau-morais', 'A1B2CE20e')
+esau_ig.login()
